7d8abbd5ff58f8a6346761ad566a2456a59f6b1c
Sang Luong <sanglt@imt-soft.com>
2021-10-10 22:26:13 +0700

    revert commit 2-1

--- src/app/admin/order-management/order-list/order-list.component.ts
+++ src/app/admin/order-management/order-list/order-list.component.ts
@@ -24,9 +24,7 @@ export class OrderListComponent implements OnInit {
   pagingResult: PagingModel = new PagingModel();
   pageCount: number;
   orders: OrderDTO[] = [];
-  testReset7: any;
-  testReset8: any;
-  testReset9: any;
+
   testReset1: any;
   testReset2: any;
   testReset3: any;
